72409945ccb87c7ed4f0541549401e49659e7561
张红宇 <545130548@qq.com>
2019-03-04 16:47:51 +0800

    fixed:aaa

---
+++ build/static/js/ipConfig.js
@@ -0,0 +1,9 @@
+/*
+* BASEURL_01 ====> http://zhr.shi.da.hao:ren/
+* */
+
+window.BASEURL_01 = "http://192.168.200.85:8085";
+window.BASEURL_02 = "http://192.168.200.166:8085";
+
+// full: 0x03, debug: 0x05
+window.resizeMode = 0x05;
